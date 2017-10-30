4cb87d64b232eef72fd22c821eb95146f7242e55
Kevin Shaffer <kevinsshaffer@gmail.com>
2017-10-29 19:00:14 -0600

    game implementation

--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
 
 #include <iostream>
+#include <string>
 #include "node.h"
+
+template<class T>
+void startGame(Node<T>* start, Node<T>* finish)
+{
+	Node<T>* location = start;
+
+	while (location != finish)
+	{
+		std::cout << location->getDescription() << std::endl;
+		std::cout << location->getDirections() << std::endl;
+
+		char input;
+		do
+		{
+			std::cout << "Which direction would you like to go?" << std::endl;
+		} while (!(std::cin >> input) || !location->tryMove(input, location));
+	}
+}
 
 int main()
 {
+	Node<char>* start = new Node<char>(NULL, NULL, NULL, NULL, 'A');
+	start->setEast(new Node<char>(NULL, NULL, NULL, NULL, 'B'));
 
+	std::cout << start->getData() << "---" << start->getEast()->getData() << std::endl;
+
+	startGame(start, start->getEast());
+
+	std::cout << "Press ctrl + z (ctrl + c) to quit." << std::endl;
+	while (std::cin) { }
 
 	return 0;
 }
+
--- node.h
+++ node.h
@@ -1,27 +1,103 @@
 /*
  * Making this a class instead of a struct.
+ * TODO: Destructors
 */
 
+#include <string>
 
-template <class T>
+template<class T>
 class Node
 {
 public:
-	Node(Node*, Node*, Node*, Node*, T);
-	T getData() const;
-	void setData(const T&);
-	Node* getNorth() const;
-	void setNorth(Node*);
-	Node* getEast() const;
-	void setEast(Node*);
-	Node* getSouth() const;
-	void setSouth(Node*);
-	Node* getWest() const;
-	void setWest(Node*);
+	Node(Node* north, Node* east, Node* south, Node* west, T data) :
+		_north(north), _east(east), _south(south), _west(west), _data(data)
+	{ }
+	T getData() const
+	{
+		return _data;
+	}
+	void setData(const T& data)
+	{
+		_data = data;
+	}
+	Node<T>* getNorth() const
+	{
+		return _north;
+	}
+	void setNorth(Node* node)
+	{
+		_north = node;
+	}
+	Node<T>* getEast() const
+	{
+		return _east;
+	}
+	void setEast(Node* node)
+	{
+		_east = node;
+	}
+	Node<T>* getSouth() const
+	{
+		return _south;
+	}
+	void setSouth(Node* node)
+	{
+		_south = node;
+	}
+	Node<T>* getWest() const
+	{
+		return _west;
+	}
+	void setWest(Node* node)
+	{
+		_west = node;
+	}
+	std::string getDescription() const
+	{
+		return "You are in a room with the a '" + std::to_string(getData()) + "' scratched into the floor.";
+	}
+	std::string getDirections() const
+	{
+		return "Would you like to go:" +
+			std::string(getNorth() != NULL ? "\n\tNorth(n)" : "") +
+			std::string(getEast() != NULL ? "\n\tEast(e)" : "") +
+			std::string(getSouth() != NULL ? "\n\tSouth(s)" : "") +
+			std::string(getWest() != NULL ? "\n\tWest(w)" : "");
+	}
+	bool tryMove(Node*& currentLoc, Node* newLoc)
+	{
+		if (newLoc)
+		{
+			currentLoc = newLoc;
+			return true;
+		}
+		else
+			return false;		
+	}
+	bool tryMove(char direction, Node*& location)
+	{
+		switch (direction)
+		{
+			case 'N':
+			case 'n':
+				return tryMove(location, getNorth());
+			case 'E':
+			case 'e':
+				return tryMove(location, getEast());
+			case 'S':
+			case 's':
+				return tryMove(location, getSouth());
+			case 'W':
+			case 'w':
+				return tryMove(location, getWest());
+			default:
+				return false;
+		}
+	}
 private:
 	T _data;
-	Node *_north;
-	Node *_east;
-	Node *_south;
-	Node *_west;
+	Node* _north;
+	Node* _east;
+	Node* _south;
+	Node* _west;
 };
